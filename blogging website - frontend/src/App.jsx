4ef00552411a21edf286ed956a77a3e07a1ae67d
iamansoni07 <aman.soni0713@gmail.com>
2024-11-26 23:39:35 +0530

    components

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar.component";
 import About from "./components/about.component";
+import Input from "./components/input.component";
 
 const App = () => {
     return (
         <>
             <Navbar />
             <About />
+            <Input />
         </>
     );
 }
