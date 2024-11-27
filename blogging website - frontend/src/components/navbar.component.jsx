3c7302e832122bc8988bf827878c5f2a47107f3e
iamansoni07 <aman.soni0713@gmail.com>
2024-11-27 15:16:12 +0530

    27nov

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,14 +1,19 @@
-import logo from "../imgs/logo.png"
-import {Link} from 'react-router-dom'
+import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
 
-const Navbar =  () => {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="flex-none w-10">
-                <img src={logo} className="w-full"/>
-            </Link>
-        </nav>
-    )   
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="flex-none w-10">
+        <img src={logo} className="w-full" />
+      </Link>
+
+      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw]">
+                                hiiiiiii
+
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
