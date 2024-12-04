5f35347e2b40f144e665215b5fde35fbb2705f6a
iamansoni07 <aman.soni0713@gmail.com>
2024-12-04 23:56:35 +0530

    .

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react"; // Import React if using a version prior to React 17
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navbar />} />
-      <Route path="/signin" element={<h1>Sign in page</h1>} />
-      <Route path="/signup" element={<h1>Sign up page</h1>} />
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+      </Route>
     </Routes>
   );
 };
