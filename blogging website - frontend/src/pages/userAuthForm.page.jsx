bbfae34f6039dc762d2f90236e073f82e0efaad9
iamansoni07 <aman.soni0713@gmail.com>
2024-12-16 23:44:38 +0530

    .

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,6 +1,15 @@
+import AnimationWrapper from "../common/page-animation";
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
+
 const UserAuthForm = ({ type }) => {
-    return <h1>{type}</h1>;
-  };
-  
-  export default UserAuthForm;
-  
+  return (
+    <AnimationWrapper>
+      <section className="h-cover flex items-center justify-center">
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default UserAuthForm;
