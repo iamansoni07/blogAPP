bb531a12d8b824594cc3a58c498574e947402dc9
iamansoni07 <aman.soni0713@gmail.com>
2024-12-17 05:15:06 +0530

    .

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
-const server = express();
-let PORT = 3000;
-
-server.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-});
