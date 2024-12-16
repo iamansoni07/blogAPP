916d56c25b0ef4006efebfdb91805eeeb1653d02
iamansoni07 <aman.soni0713@gmail.com>
2024-12-16 23:49:34 +0530

    .

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 } }) => {
+const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1 } }) => {
   return (
     <motion.div
       initial={initial}
