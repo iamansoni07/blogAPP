3e245c79c5a360e8a23fd2a0f3655754f857eb17
iamansoni07 <aman.soni0713@gmail.com>
2024-12-16 23:46:48 +0530

    .

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,10 +1,14 @@
-const AnimationWrapper = ({ children }) => {
-    return (
-      <div>
-        {children}
-      </div>
-    );
-  };
-  
-  export default AnimationWrapper;
-  
+import { AnimatePresence, motion } from "framer-motion";
+
+const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate }) => {
+  return (
+    <motion.div
+      initial={initial}
+      animate={animate}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimationWrapper;
