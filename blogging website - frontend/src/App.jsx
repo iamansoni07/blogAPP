81ef9edfa97b704bb418d994ca151dac81a48165
iamansoni07 <aman.soni0713@gmail.com>
2025-01-12 23:46:42 +0530

    update app.jsx

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useState } from "react";
+
+export const UserContext = createContext({});
 
 const App = () => {
+  const [userAuth, setUserAuth] = useState();
+
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-      </Route>
-    </Routes>
+    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 };
-
-export default App;
