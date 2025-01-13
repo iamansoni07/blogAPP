c695c3ffaaaebf4535b7aebf3d56433946734f42
iamansoni07 <aman.soni0713@gmail.com>
2025-01-13 23:29:55 +0530

    update app.jsx

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
 
 export const UserContext = createContext({});
 
 const App = () => {
-  const [userAuth, setUserAuth] = useState();
+  const [userAuth, setUserAuth] = useState({});
+
+  useEffect(() => {
+    let userInSession = lookInSession("user");
+    userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
+  }, []);
 
   return (
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
